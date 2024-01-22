b10c57ad08f19ebf477fd1bb84b900dd9b2877cf
UltimateToaster <polikarpovzakhar@gmail.com>
2024-01-22 21:21:44 +0100

    Adjusted Vite config  for Github pages

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,7 @@
 import { defineConfig } from 'vite'
 import htmlPurge from 'vite-plugin-purgecss';
 import react from '@vitejs/plugin-react-swc'
+import path from 'path'
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -8,4 +9,11 @@ export default defineConfig({
     react(),
     htmlPurge([htmlPurge()]),
   ],
+	base: '/qr-code-component/',
+	
+	resolve: {
+		alias: {
+			'@': path.resolve(__dirname, './src')
+		}
+	}
 })
