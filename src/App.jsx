ded9b9734d98b3840de03d36ee0c2abc16d906e6
UltimateToaster <polikarpovzakhar@gmail.com>
2024-01-22 21:46:23 +0100

    Fix for image in deployment

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,7 @@ function App() {
 				<section className="section" data-type="full-screen">
 					<div className="container">
 						<div className="card | mx-auto" data-type="qr-code">
-							<img src="src/assets/image-qr-code.png" alt="An image of a QR-Code" />
+							<img src="./image-qr-code.png" alt="An image of a QR-Code" />
 							<div className="text-center flow padding-block-end-6">
 								<h3  className="card__heading">Improve your front-end skills by building projects</h3>
 								<p>Scan the QR code to visit Frontend Mentor and take your coding skills to the next level</p>
